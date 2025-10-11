e715b434270809ccfa4733805bfaf0e01ed88e4c
Mathew Dewes <mathewdewes01@gmail.com>
2025-10-11 14:42:02 +1300

    Images, tailwind global styles and navbar added

---
+++ components/BeachBanner.tsx
@@ -0,0 +1,8 @@
+export default function BeachBanner({text}:{text: string}){
+    return (
+           <div className="bg-[url('/beach.jpg')] h-50 bg-cover bg-center p-8 mb-2">
+         <h1 className="text-xl lg:text-2xl font-medium">{text}</h1>
+      </div>
+    )
+}
+
---
+++ components/Button.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+type ButtonProps = {
+  text?: string;
+  danger?: boolean;
+  onClick?: () => void;
+};
+
+export default function Button({text = "Hello", danger = false, onClick}:ButtonProps){
+
+    return (
+        <button onClick={onClick} className={`px-2 py-2 rounded font-semibold cursor-pointer ${danger ? "bg-red-400 hover:bg-red-600" : "bg-sky-400 hover:bg-sky-600"}`}>{text}</button>
+    )
+}
